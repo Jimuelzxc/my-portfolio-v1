3f798796c0eb64502cfeb777e71eaf0abd5478bc
Jimuelzxc <jimuelmarcgensaya@gmail.com>
2022-10-25 13:44:44 +0800

    new data

--- src/data.js
+++ src/data.js
@@ -25,6 +25,10 @@ export const IconsData = {
 export const ProjectsData = {
     tech: [
 				{
+						thumbnail: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs3/281084269/original/1d5fa0fc0bd2746ee7ae7ea39eda8c367229194b/create-landing-page-or-website-for-your-business-website.png"
+				},
+
+				{
 						thumbnail: "https://raw.githubusercontent.com/Jimuelzxc/discord-shorts/main/src/assets/discord-shorts.jpg",
 						link: "https://jimuelzxc.github.io/discord-shorts"
 				},
@@ -46,9 +50,6 @@ export const ProjectsData = {
         }
     ],
     design: [
-				{
-						thumbnail: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs3/281084269/original/1d5fa0fc0bd2746ee7ae7ea39eda8c367229194b/create-landing-page-or-website-for-your-business-website.png"
-				},
         {
             thumbnail: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bf3abc154772089.63481f39666ec.jpg",
             link: "https://www.behance.net/gallery/154772089/Cloud9"
